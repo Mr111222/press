bb1c6570044c1d7bbbaed6aa374c6b933d4b4ab2
zhouzhi <icitic-zhouzhi@csmail.citicbank.com>
2019-05-13 10:21:31 +0800

    2019-05-13-10-21-31

--- build/release.sh
+++ build/release.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env sh
 set -e
 
-# git checkout master
-# git merge dev
-
 VERSION=`npx select-version-cli`
 
 read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  echo "Releasing $VERSION ..."
-
   # build
   VERSION=$VERSION
 
@@ -26,3 +21,17 @@ then
     npm publish
   fi
 fi
+
+sleep 2
+
+clear || clsg
+
+git status
+
+git add -A
+
+git commit -m "xxxx"
+
+git push origin master
+
+
