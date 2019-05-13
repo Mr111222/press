9eeb3a6245e36490d6a7f95c94c96478dcaf61e8
zhouzhi <icitic-zhouzhi@csmail.citicbank.com>
2019-05-13 09:08:12 +0800

    2019-05-13-09-08-12

--- build/release.sh
+++ build/release.sh
@@ -4,20 +4,13 @@ set -e
 # git checkout master
 # git merge dev
 
-VERSION=`npx select-version-cli`
+echo "输入版本"
 
-read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
-echo    # (optional) move to a new line
+read $VERSION
+
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   echo "Releasing $VERSION ..."
-
-  # build
-  # VERSION=$VERSION npm run dist
-
-  # publish theme
-  echo "Releasing theme-chalk $VERSION ..."
-  cd packages/theme-chalk
   npm version $VERSION --message "[release] $VERSION"
   if [[ $VERSION =~ "beta" ]]
   then
