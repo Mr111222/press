e1ee64a2438dea7d45f0a6a3cc7efe8357149c37
zhouzhi <icitic-zhouzhi@csmail.citicbank.com>
2019-05-13 14:26:34 +0800

    2019-05-13-14-26-34

--- build/release.sh
+++ build/release.sh
@@ -1,18 +1,31 @@
 #!/usr/bin/env sh
 set -e
 
+# git checkout master
+# git merge dev
+
 VERSION=`npx select-version-cli`
 
 read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
-
+echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+  echo "Releasing $VERSION ..."
+
   # build
-  VERSION=$VERSION
+  VERSION=$VERSION 
 
-  echo "Releasing $VERSION ......"
-
+  # commit
+  git add -A
+  git commit -m "[build] $VERSION"
   npm version $VERSION --message "[release] $VERSION"
+
+  # publish
+  git push eleme master
+  git push eleme refs/tags/v$VERSION
+  git checkout dev
+  git rebase master
+  git push eleme dev
 
   if [[ $VERSION =~ "beta" ]]
   then
@@ -21,11 +34,3 @@ then
     npm publish
   fi
 fi
-
-sleep 2
-
-clear || cls
-
-git push origin master
-
-
