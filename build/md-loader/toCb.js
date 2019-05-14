d5e770607ef7426b0ba727ddcb0eb670dc09135f
zhouzhi <icitic-zhouzhi@csmail.citicbank.com>
2019-05-14 15:07:03 +0800

    2019-05-14-15-07-03

--- build/md-loader/toCb.js
+++ build/md-loader/toCb.js
@@ -1,7 +1,4 @@
-const utils = require('loader-utils')
-const fs = require('fs')
-let reg = /<template[^>]*>([\s\S]*)<\/template>/;
-module.exports = function (source, map){
+module.exports = function(source, map) {
 	let s = source.replace(/el-/g, 'cb-').replace(/El-/g, 'Cb-')
 	return s
 }
