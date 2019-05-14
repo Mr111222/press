bd878ae8a9e41a6bd712809e087b9106ec065b78
zhouzhi <icitic-zhouzhi@csmail.citicbank.com>
2019-05-14 15:31:48 +0800

    2019-05-14-15-31-47

--- src/index.js
+++ src/index.js
@@ -184,8 +184,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '2.8.2',
-  pps: 'entry file',
+  version: '0.0.1',
   locale: locale.use,
   i18n: locale.i18n,
   install,
