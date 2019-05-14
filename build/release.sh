7fb0a5f54dcbc75eba06ce5ea3838c72f6483de1
zhouzhi <icitic-zhouzhi@csmail.citicbank.com>
2019-05-14 17:28:06 +0800

    2019-05-14-17-28-06

--- build/release.sh
+++ build/release.sh
@@ -56,6 +56,19 @@ then
 
   npm version $VERSION --message "[release] $VERSION"
 
+
+  # commit
+    git add -A
+    git commit -m "[build] $VERSION"
+    npm version $VERSION --message "[release] $VERSION"
+
+    # publish
+    git push origin master
+    git push origin refs/tags/v$VERSION
+    git checkout dev
+    git rebase master
+    git push origin dev
+
   # if [[ $VERSION =~ "beta" ]]
   # then
   #   npm publish --tag beta
@@ -64,7 +77,7 @@ then
   # fi
 fi
 
-git push origin master
+# git push origin master
 
 # sleep 2
 
