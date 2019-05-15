a33eb813b60b4db32379f537d02663dccd2d943a
zhouzhi <icitic-zhouzhi@csmail.citicbank.com>
2019-05-15 10:02:13 +0800

    2019-05-15-10-02-13

--- build/release.sh
+++ build/release.sh
@@ -1,27 +1,24 @@
-
 #!/usr/bin/env sh
 set -e
 
 git checkout master
-
 git merge dev
-
-git push origin master
 
 VERSION=`npx select-version-cli`
 
 read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
-
+echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+  echo "Releasing $VERSION ..."
+
   # build
-  VERSION=$VERSION 
-  npm run dist
+  VERSION=$VERSION npm run dist
 
   # publish theme
-  echo "Releasing theme-chalk $VERSION ..........."
+  echo "Releasing theme-chalk $VERSION ..."
   cd packages/theme-chalk
-  echo "enter theme-chalk"
+  echo 'enter theme-chalk...'
   npm version $VERSION --message "[release] $VERSION"
   if [[ $VERSION =~ "beta" ]]
   then
@@ -30,17 +27,12 @@ then
     npm publish
   fi
   cd ../..
-  echo "enter root"
+  echo 'enter root...'
   # commit
   git add -A
   git commit -m "[build] $VERSION"
   npm version $VERSION --message "[release] $VERSION"
-  if [[ $VERSION =~ "beta" ]]
-  then
-    npm publish --tag beta
-  else
-    npm publish
-  fi
+
   # publish
   git push origin master
   git push origin refs/tags/v$VERSION
@@ -55,10 +47,3 @@ then
     npm publish
   fi
 fi
-
-
-
-
-
-
-
